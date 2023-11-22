1d0eca766a89fe741d8e5cc7a050f5768531cbd4
Roman Khabayev <supernaturalmlady@gmail.com>
2023-11-22 15:51:59 +0600

    logout added, models fixed

--- server/models/user-model.js
+++ server/models/user-model.js
@@ -3,7 +3,7 @@ const { Schema, model } = require('mongoose')
 const UserSchema = new Schema({
   email: { type: String, unique: true, required: true },
   password: { type: String, required: true },
-  isActivated: { type: Boolean, required: false },
+  isActivated: { type: Boolean, default: false },
   activationLink: { type: String },
 })
 
